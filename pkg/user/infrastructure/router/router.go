8d3abcfcc8a6d2b7920100ccc297ce25948b0f3e
juancathree <juancarloslucenamonje@gmail.com>
2021-10-18 11:27:43 +0100

    add user router

---
+++ pkg/user/infrastructure/router/router.go
@@ -0,0 +1,12 @@
+package router
+
+import (
+	"citywalker/pkg/user/infrastructure/handler"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func Router(router fiber.Router) {
+	router.Post("/login", handler.Login())
+	router.Post("/signup", handler.Signup())
+}
