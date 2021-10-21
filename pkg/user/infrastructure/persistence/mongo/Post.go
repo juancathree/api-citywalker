0a31a2b775587f922064cc719d348d734a7f424c
juancathree <juancarloslucenamonje@gmail.com>
2021-10-22 00:26:40 +0100

    modify return statements

--- pkg/user/infrastructure/persistence/mongo/Post.go
+++ pkg/user/infrastructure/persistence/mongo/Post.go
@@ -6,12 +6,12 @@ import (
 	"fmt"
 )
 
-func (mo *MongoRepository) Post(user *domain.User) (*domain.User, error) {
+func (mo *MongoRepository) Post(user *domain.User) error {
 	// Inserting user to database
 	_, err := mo.Collection.InsertOne(context.Background(), *user)
 	if err != nil {
-		return nil, fmt.Errorf("database error: couldn't be posted user to database")
+		return fmt.Errorf("database error: couldn't be posted user to database")
 	}
 
-	return user, nil
+	return nil
 }
