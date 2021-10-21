ec349b7f082048978888a87cc1901118da6a54dc
juancathree <juancarloslucenamonje@gmail.com>
2021-10-22 00:26:19 +0100

    modify returns variables

--- pkg/user/application/Signup.go
+++ pkg/user/application/Signup.go
@@ -3,15 +3,13 @@ package application
 import (
 	"citywalker/pkg/user/domain"
 	"citywalker/shared"
-	"fmt"
 )
 
-func Signup(user *domain.User) (*domain.User, error) {
+func Signup(user *domain.User) error {
 
 	err := shared.Validate.Struct(user)
 	if err != nil {
-		fmt.Println(err)
-		return nil, err
+		return err
 	}
 	// Set password
 	user.SetPassword(&user.Password)
--- pkg/user/domain/Repository.go
+++ pkg/user/domain/Repository.go
@@ -2,6 +2,6 @@ package domain
 
 type Repository interface {
 	Get(userID *string) (*User, error)
-	Post(user *User) (*User, error)
+	Post(user *User) error
 	PostTravel(userID *string, travelID *string) error
 }
