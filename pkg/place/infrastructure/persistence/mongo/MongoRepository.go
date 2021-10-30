b4873a89746030c24a77fe6e580bf613038debbc
juancathree <juancarloslucenamonje@gmail.com>
2021-10-30 16:42:09 +0100

    mongorepository of place

---
+++ pkg/place/infrastructure/persistence/mongo/MongoRepository.go
@@ -0,0 +1,19 @@
+package persistence
+
+import (
+	"citywalker/database"
+	"citywalker/pkg/place/domain"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type MongoRepository struct {
+	Collection *mongo.Collection
+}
+
+func NewMongoRepository() domain.Repository {
+	return &MongoRepository{
+		Collection: database.DB.(*mongo.Database).Collection(os.Getenv("DB_COLLECTION_PLACES")),
+	}
+}
