b76991e0e2e45d3c11b824bfa3f53a5286e0a2ab
juancathree <juancarloslucenamonje@gmail.com>
2021-10-30 16:53:01 +0100

    create expense's domain

---
+++ pkg/expenses/domain/Expenses.go
@@ -0,0 +1,15 @@
+package domain
+
+type Expenses map[string][]Expense
+
+type Expense struct {
+	Amount      float64 `validate:"required" json:"amount" bson:"amount"`
+	IsPersonal  bool    `validate:"required" json:"isPersonal" bson:"isPersonal"`
+	Category    string  `json:"category" bson:"category"`
+	Description string  `validate:"required,max=30" json:"description" bson:"description"`
+}
+
+func (e *Expenses) Initialize(userID *string) {
+	*e = make(Expenses)
+	(*e)[*userID] = make([]Expense, 0)
+}
