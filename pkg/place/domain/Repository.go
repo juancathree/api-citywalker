455ef077261ce9b7e68cfd869a9e6af13110de30
juancathree <juancarloslucenamonje@gmail.com>
2021-11-04 02:48:00 +0000

    add lng variable

--- pkg/place/domain/Repository.go
+++ pkg/place/domain/Repository.go
@@ -2,6 +2,6 @@ package domain
 
 type Repository interface {
 	Gets(city *string, lng *string) (*[]Place, error)
-	GetsCustom(city *string, pref, cuV *[]string) (*[]Place, error)
+	GetsCustom(city *string, pref, cuV *[]string, lng *string) (*[]Place, error)
 	GetCities(lng *string) (*[]string, error)
 }
