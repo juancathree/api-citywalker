83ae96ef302667242534db17279a3cf236e1e901
juancathree <juancarloslucenamonje@gmail.com>
2021-11-04 02:45:23 +0000

    add lng variable

--- pkg/place/application/GetsCustom.go
+++ pkg/place/application/GetsCustom.go
@@ -2,6 +2,6 @@ package application
 
 import "citywalker/pkg/place/domain"
 
-func GetsCustom(city *string, pref, cuV *[]string) (*[]domain.Place, error) {
-	return repo.GetsCustom(city, pref, cuV)
+func GetsCustom(city *string, pref, cuV *[]string, lng *string) (*[]domain.Place, error) {
+	return repo.GetsCustom(city, pref, cuV, lng)
 }
